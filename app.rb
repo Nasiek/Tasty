de35c5d4b88012ac80c0c391f429f71c75fe8e2d
k <keisangittens@gmail.com>
2018-04-09 11:24:47 -0400

    input boxes re-arranged.

--- app.rb
+++ app.rb
@@ -32,3 +32,9 @@ post 'user/signin' do
 	
 	params.to_s
 end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+User(id: integer, fname: string, lname: string, city: string, email: string)
+user_list = [
+["Keisan","Gittens", "Philadelphia", "keisangittens@gmail.com"],
+["Jon", "Snow","Winterfell","itsyaboi_jonsnow@gmail.com"],
+["Bernard","Lowe","Westworld","am_i_a_robot@gail.com"],
+["Robert","Ford","Margam","robert_ford_god@gmail.com"],
+[""""""""]
+	]]
+
+
+
+
+
+
+Blog(id: integer, blogpost: text, date_created: datetime)
