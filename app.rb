efa1ab783b2bb4d8e862fb39bc9f3f625a2b0810
k <keisangittens@gmail.com>
2018-04-10 18:55:19 -0400

    added sign out, sign up functionality. set up delete and edit records functionality

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'shotgun'
 require 'sinatra/activerecord'
+require 'sinatra/flash'
+
 enable :sessions
 set :database, "sqlite3:tastydatabase.sqlite3"
 require './models.rb'
@@ -9,6 +11,33 @@ def current_user
 	if session[:user_id]
 		@current_user = User.find(session[:user_id])
 end
+end
+
+post '/update_blog' do
+redirect '/update_blog_page'
+end
+
+get '/update_blog' do
+@blogs = Blog.all
+erb :Edit_blogPost
+end
+
+post '/editpost' do
+redirect '/MyProfile'
+end	
+
+post '/update_info' do
+redirect '/update_info'
+end
+
+get '/update_info' do
+@users = User.all
+
+erb :Edit_infoForm
+end
+
+before do
+	current_user
 end
 
 get '/Homepage' do
@@ -21,7 +50,7 @@ get '/Newsfeed' do
 	@users = User.all
 	@blogpost = Blog.all
 
-	erb :Pesrsonal_Homepage
+	erb :Personal_Homepage
 end
 
 get '/MyProfile' do
@@ -29,7 +58,7 @@ get '/MyProfile' do
 	erb :User_Profile
 end
 
-get '/UserProfile/:id' do
+get '/UserProfile/id' do
 	@user = User.find(params[:id])
 	erb :Other_Users_Profile
 end
@@ -43,6 +72,26 @@ post '/user/signup' do
 	params[:city]
 	params[:password]
 	puts params.to_s
+	
+
+	session[:user_id] = User.last.id
+	redirect '/Newsfeed'
+
+	# @user = User.where(uname: params[:uname]).first
+ #    puts "aaaaaaaaaaaaaaaaaaaaaaa"
+ #    puts @user.uname
+ #    puts "aaaaaaaaaaaaaaaaaaaaaaa"
+
+ #    if @user.password == params[:password]
+        
+ #        # flash[:notice] = "Success!"
+ #        redirect '/Newsfeed'
+ #    else
+ #        flash[:notice] = "FAILED LOGIN :("
+ #        redirect '/Homepage'
+ #    end
+
+	
 	end
 
 # post '/user/signin' do
@@ -52,14 +101,31 @@ post '/user/signup' do
 # 	params[:password]
 # end
 
-post 'blog/post' do
+post '/create_message' do
 Blog.create(params[:create])
-params[:category]
-params[:title]
-params[:blogpost]
-params[:user_id]
-# @blogpost : Blog.create
+redirect '/Newsfeed'
 end
+
+post '/sign-out' do
+ session[:user_id] = nil
+   redirect '/Homepage'
+ end
+
+put '/update_blog' do
+edit_blog = 
+end
+ 
+put '/update_profile' do
+
+edit_profile = @current_user
+edit_profile.uname = params[:uname]
+edit_profile.fname = params[:fname]
+edit_profile.lname = params[:lname]
+edit_profile.city = params[:city]
+edit_profile.email = params[:email]
+edit_profile.password = params[:password]
+end
+
 
 post '/user/signin' do
     @user = User.where(uname: params[:uname]).first
@@ -73,7 +139,7 @@ post '/user/signin' do
         redirect '/Newsfeed'
     else
         flash[:notice] = "FAILED LOGIN :("
-        redirect '/sign-in-failed'
+        redirect '/Homepage'
     end
 end
 
